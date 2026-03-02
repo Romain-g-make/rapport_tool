0b5d09a36716567e9786d17fc57d1b77cc232bc0
guromain <romain.guibert44@ynov.com>
2026-03-02 11:54:24 +0100

    structuration du projet

--- fonction_go/GetParams.go
+++ fonction_go/GetParams.go
@@ -1,41 +1,14 @@
 package fonction_go
-package fonction_go
 
 import (
 	"net/http"
-	"strconv"
 )
 
 type queryParams struct {
-	pagination  int
-	page        int
-	searchQuery string
-	searchType  string
-	artistName  string
-	reset       string
-	sortOrder   string
 }
 
 func extractQueryParams(r *http.Request) queryParams {
-	params := queryParams{
-		searchQuery: r.URL.Query().Get("Search"),
-		searchType:  r.URL.Query().Get("SearchType"),
-		artistName:  r.URL.Query().Get("Is_artist"),
-		reset:       r.URL.Query().Get("Reset"),
-		sortOrder:   r.URL.Query().Get("sort"),
-	}
-
-	if p := r.URL.Query().Get("pagination"); p != "" {
-		if val, err := strconv.Atoi(p); err == nil && val > 0 {
-			params.pagination = val
-		}
-	}
-
-	if pa := r.URL.Query().Get("page"); pa != "" {
-		if val, err := strconv.Atoi(pa); err == nil && val >= 0 {
-			params.page = val
-		}
-	}
+	params := queryParams{}
 
 	return params
 }
