d98854a53c30f185d19e62c9fdeb889831f1ef13
timeoblrynov <timeo.baffreauleroux@ynov.com>
2026-03-22 17:04:41 +0100

    pdf try

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from fpdf import FPDF
+
+pdf = FPDF()
+
+pdf.add_page()
+pdf.set_font("Arial", size=16)
+
+pdf.cell(22, 10, txt="Ceci est un test", align="C")
+
+pdf.output("test.pdf")
